5e2b14b5b0ccb3297f84c4808b9db90313dcb6ed
simson2010 <simson2010@hotmail.com>
2011-10-24 00:11:46 +0800

    add Mono.Cecil for modifying anonymous type as public.

--- JQueryMobileSite1/JQueryMobileSite1/Controllers/HomeController.cs
+++ JQueryMobileSite1/JQueryMobileSite1/Controllers/HomeController.cs
@@ -6,10 +6,26 @@ using System.Web.Mvc;
 
 namespace jQueryMobileTemplate.Controllers
 {
+    public class QuModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PostModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            ViewBag.Home = new { Title = "MSF Home" };
+            ViewBag.SureyMastList = new[] { new QuModel { ID = 1, Name = "Qu1" }, new QuModel { ID = 2, Name = "Qu2" } };
+            ViewBag.Post = (new { Title = "First Post", Content = "Post Content" });
+
             return View();
         }
 
